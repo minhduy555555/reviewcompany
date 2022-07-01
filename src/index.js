c7ed613db7ed0698544086a4fcb7a298e60a25a5
LuuManhCuong <88231657+LuuManhCuong@users.noreply.github.com>
2022-07-01 18:08:28 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+const path = require('path')
+const { engine } = require('express-handlebars')
 const port = 3000
 
+app.use(express.static(path.join(__dirname,'public')))
+// cấu hình hbs
+app.engine('hbs', engine({
+  extname: '.hbs',
+  helpers: {
+    sum: (a,b) => a+b, 
+  }
+}
+));
+app.set("view engine", "hbs")
+app.set('views', path.join(__dirname, 'resource','views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("home")
 })
 
 app.listen(port, () => {
